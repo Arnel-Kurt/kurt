36a14b13939cbe11b646482ceb2b5de312426afa
Arnel-Kurt <arnelkurtmanlaibanez@gmail.com>
2024-04-23 16:32:37 +0800

    hays

--- src/db/tbl_user.sql
+++ src/db/tbl_user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2024 at 10:01 AM
+-- Generation Time: Apr 23, 2024 at 10:31 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,16 +18,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ibanez`
+-- Database: `ker`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nel`
+-- Table structure for table `tbl_user`
 --
 
-CREATE TABLE `nel` (
+CREATE TABLE `tbl_user` (
   `u_id` int(20) NOT NULL,
   `u_fname` varchar(50) NOT NULL,
   `u_lname` varchar(50) NOT NULL,
@@ -39,20 +39,21 @@ CREATE TABLE `nel` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `nel`
+-- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `nel` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_username`, `u_password`, `u_type`, `u_status`) VALUES
-(1, 'arnel', 'ibanez', 'arnelibanez@gmail.com', 'nel', '12345678', 'Admin', 'Active');
+INSERT INTO `tbl_user` (`u_id`, `u_fname`, `u_lname`, `u_email`, `u_username`, `u_password`, `u_type`, `u_status`) VALUES
+(1, '', '', '', '', '', '', ''),
+(2, 'arnel', 'ibanez', 'arnelibanez@gmail.com', 'nel', '12345678', 'Admin', 'Active');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `nel`
+-- Indexes for table `tbl_user`
 --
-ALTER TABLE `nel`
+ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`u_id`);
 
 --
@@ -60,10 +61,10 @@ ALTER TABLE `nel`
 --
 
 --
--- AUTO_INCREMENT for table `nel`
+-- AUTO_INCREMENT for table `tbl_user`
 --
-ALTER TABLE `nel`
-  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `tbl_user`
+  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
